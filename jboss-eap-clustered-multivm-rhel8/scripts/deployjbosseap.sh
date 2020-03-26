a5c352b7bedfbda3eadc3c5832dbcda5a8ca9c92
Suraj Pai <suraj.rpai2093@gmail.com>
2020-03-26 15:25:58 +0530

    Update deployjbosseap.sh

--- jboss-eap-clustered-multivm-rhel8/scripts/deployjbosseap.sh
+++ jboss-eap-clustered-multivm-rhel8/scripts/deployjbosseap.sh
@@ -33,7 +33,7 @@ echo "Install openjdk, wget, git, unzip, vim"  >> /home/$1/install.log
 sudo yum install java-1.8.0-openjdk wget unzip vim git -y
 
 echo "Downlaod jboss-eap-7.2"  >> /home/$1/install.log 
-wget https://vmuagstore.blob.core.windows.net/software/jboss-eap-7.2.0.zip
+wget https://experienceazure.blob.core.windows.net/templates/EAP7.2/jboss-eap-7.2.0.zip
 
 echo "unzip jboss-eap"  >> /home/$1/install.log 
 
@@ -56,8 +56,14 @@ echo "start jboss server" >> /home/$1/install.log
 
 $JBOSS_HOME/bin/standalone.sh -bprivate $IP_ADDR --server-config=standalone-azure-ha.xml -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=$CONTAINER_NAME -Djava.net.preferIPv4Stack=true &
 
+echo "export JBOSS_HOME="/opt/rh/jboss-eap-7.2/"" >>/bin/jbossservice.sh
 echo "$JBOSS_HOME/bin/standalone.sh -bprivate $IP_ADDR --server-config=standalone-azure-ha.xml -Djboss.jgroups.azure_ping.storage_account_name=$STORAGE_ACCOUNT_NAME -Djboss.jgroups.azure_ping.storage_access_key=$STORAGE_ACCESS_KEY -Djboss.jgroups.azure_ping.container=$CONTAINER_NAME -Djava.net.preferIPv4Stack=true &" > /bin/jbossservice.sh
 chmod +x /bin/jbossservice.sh
+
+yum install cronie cronie-anacron
+service crond start
+chkconfig crond on
+echo "@reboot sleep 90 && /bin/jbossservice.sh" >>  /etc/crontab
 
 echo "deploy an applicaiton " >> /home/$1/install.log
 git clone https://github.com/danieloh30/eap-session-replication.git
