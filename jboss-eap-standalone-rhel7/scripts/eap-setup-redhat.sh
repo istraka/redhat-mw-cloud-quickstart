d353a0faeba48272b8fca9f3d2b6323c06cd5855
Suraj Pai <suraj.rpai2093@gmail.com>
2020-04-16 20:11:59 +0530

    Update eap-setup-redhat.sh

--- jboss-eap-standalone-rhel7/scripts/eap-setup-redhat.sh
+++ jboss-eap-standalone-rhel7/scripts/eap-setup-redhat.sh
@@ -2,32 +2,40 @@
 
 # $1 - VM Host User Name
 
-/bin/date +%H:%M:%S > /home/$1/install.progress.txt
-echo "ooooo      REDHAT EAP7.2 RPM INSTALL      ooooo" >> /home/$1/install.progress.txt
+/bin/date +%H:%M:%S >> /home/$1/install.progress.txt
+echo "ooooo      RED HAT JBoss EAP7.2 RPM INSTALL      ooooo" >> /home/$1/install.progress.txt
 
 export EAP_HOME="/opt/rh/eap7/root/usr/share/wildfly"
+export EAP_RPM_CONF_STANDALONE="/etc/opt/rh/eap7/wildfly/eap7-standalone.conf"
 EAP_USER=$2
 EAP_PASSWORD=$3
 RHSM_USER=$4
 RHSM_PASSWORD=$5
 export RHSM_POOL=$6
+export RHEL_POOL=$7
 
-PROFILE=standalone 
-echo "EAP admin user"+${EAP_USER} >> /home/$1/install.progress.txt
-echo "Initial EAP7.2 setup" >> /home/$1/install.progress.txt
+PROFILE=standalone
+echo "JBoss EAP admin user"+${EAP_USER} >> /home/$1/install.progress.txt
+echo "Initial JBoss EAP7.2 setup" >> /home/$1/install.progress.txt
 subscription-manager register --username $RHSM_USER --password $RHSM_PASSWORD  >> /home/$1/install.progress.txt 2>&1
+subscription-manager attach --pool=${RHEL_POOL} >> /home/$1/install.progress.txt 2>&1
 subscription-manager attach --pool=${RHSM_POOL} >> /home/$1/install.progress.txt 2>&1
 echo "Subscribing the system to get access to EAP 7.2 repos" >> /home/$1/install.progress.txt
 
-# Install EAP7.2 
+# Install JBoss EAP7.2 
 subscription-manager repos --enable=jb-eap-7-for-rhel-7-server-rpms >> /home/$1/install.out.txt 2>&1
 yum-config-manager --disable rhel-7-server-htb-rpms
 
-echo "Installing EAP7.2 repos" >> /home/$1/install.progress.txt
+echo "Installing JBoss EAP7.2 repos" >> /home/$1/install.progress.txt
 yum groupinstall -y jboss-eap7 >> /home/$1/install.out.txt 2>&1
 
-echo "Enabling EAP7.2 service" >> /home/$1/install.progress.txt
+echo "Enabling JBoss EAP7.2 service" >> /home/$1/install.progress.txt
 systemctl enable eap7-standalone.service
+
+echo "Configure JBoss EAP 7.2 RPM file" >> /home/$1/install.progress.txt
+
+echo "WILDFLY_SERVER_CONFIG=standalone-full.xml" >> ${EAP_RPM_CONF_STANDALONE}
+echo 'WILDFLY_OPTS="-Djboss.bind.address.management=0.0.0.0"' >> ${EAP_RPM_CONF_STANDALONE}
 
 echo "Installing GIT" >> /home/$1/install.progress.txt
 yum install -y git >> /home/$1/install.out.txt 2>&1
@@ -38,10 +46,10 @@ git clone https://github.com/MyriamFentanes/dukes.git >> /home/$1/install.out.tx
 mv /home/$1/dukes/target/dukes.war $EAP_HOME/standalone/deployments/dukes.war
 cat > $EAP_HOME/standalone/deployments/dukes.war.dodeploy
 
-echo "Configuring EAP managment user" >> /home/$1/install.progress.txt
+echo "Configuring JBoss EAP management user" >> /home/$1/install.progress.txt
 $EAP_HOME/bin/add-user.sh -u $EAP_USER -p $EAP_PASSWORD -g 'guest,mgmtgroup'
 
-echo "Start EAP 7.2" >> /home/$1/install.progress.txt
+echo "Start JBoss EAP 7.2" >> /home/$1/install.progress.txt
 systemctl restart eap7-standalone.service 
 
 # Open Red Hat software firewall for port 8080 and 9990:
