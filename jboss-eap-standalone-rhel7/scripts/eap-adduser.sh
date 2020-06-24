4903a58086e8afa8bdb666ea0e5808a273a1c1a9
Suraj Pai <suraj.rpai2093@gmail.com>
2020-06-24 17:20:22 +0530

    Update eap-adduser.sh

--- jboss-eap-standalone-rhel7/scripts/eap-adduser.sh
+++ jboss-eap-standalone-rhel7/scripts/eap-adduser.sh
@@ -15,12 +15,7 @@ JBOSS_EAP_PASSWORD=$2
 echo "Configuring JBoss EAP management user" | adddate >> adduser.log
 echo "$EAP_HOME/bin/add-user.sh -u JBOSS_EAP_USER -p JBOSS_EAP_PASSWORD -g 'guest,mgmtgroup'" | adddate >> adduser.log
 $EAP_HOME/bin/add-user.sh -u $JBOSS_EAP_USER -p $JBOSS_EAP_PASSWORD -g 'guest,mgmtgroup' >> adduser.log 2>&1
-flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP management user configuration Failed" | adddate >> adduser.log; exit $flag;  fi
-
-echo "Start JBoss-EAP service" | adddate >> adduser.log
-echo "$EAP_HOME/bin/standalone.sh -c standalone-full.xml -b $IP_ADDR -bmanagement $IP_ADDR &" | adddate >> adduser.log
-$EAP_HOME/bin/standalone.sh -c standalone-full.xml -b $IP_ADDR -bmanagement $IP_ADDR & >> adduser.log 2>&1
-flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Starting JBoss EAP service Failed" | adddate >> adduser.log; exit $flag;  fi 
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP management user configuration Failed" | adddate >> adduser.log; exit $flag;  fi 
 
 # Seeing a race condition timing error so sleep to deplay
 sleep 20
