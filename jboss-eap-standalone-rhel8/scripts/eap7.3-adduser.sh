c4e564eb402e3cce5921c3b6df486bcfd37d828c
Suraj Pai <suraj.rpai2093@gmail.com>
2020-07-14 21:36:16 +0530

    Create eap7.3-adduser.sh

---
+++ jboss-eap-standalone-rhel8/scripts/eap7.3-adduser.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+adddate() {
+    while IFS= read -r line; do
+        printf '%s %s\n' "$(date "+%Y-%m-%d %H:%M:%S")" "$line";
+    done
+}
+
+export EAP_HOME="/opt/rh/eap7/root/usr/share/wildfly"
+
+JBOSS_EAP_USER=$1
+JBOSS_EAP_PASSWORD=$2
+
+/bin/date +%H:%M:%S >> adduser.log
+echo "Configuring JBoss EAP management user" | adddate >> adduser.log
+echo "$EAP_HOME/bin/add-user.sh -u JBOSS_EAP_USER -p JBOSS_EAP_PASSWORD -g 'guest,mgmtgroup'" | adddate >> adduser.log
+$EAP_HOME/bin/add-user.sh -u $JBOSS_EAP_USER -p $JBOSS_EAP_PASSWORD -g 'guest,mgmtgroup' >> adduser.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP management user configuration Failed" | adddate >> adduser.log; exit $flag;  fi 
+
+# Seeing a race condition timing error so sleep to deplay
+sleep 20
+
+echo "ALL DONE!" | adddate >> adduser.log
+/bin/date +%H:%M:%S >> adduser.log
