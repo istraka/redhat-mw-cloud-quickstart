838aa1dd755bc2590b01a690997336e64235a6b5
Suraj Pai <suraj.rpai2093@gmail.com>
2020-08-12 16:52:03 +0530

    Update eap-adduser.sh

--- jboss-eap-standalone-rhel8/scripts/eap-adduser.sh
+++ jboss-eap-standalone-rhel8/scripts/eap-adduser.sh
@@ -16,8 +16,8 @@ RHSM_PASSWORD=$4
 RHSM_POOL=$5
 PLAN=${6}
 
-echo "JBoss plan : " ${PLAN} | adddate >> eap.log
-if [ ${PLAN} == "JBoss-EAP7.2-on-RHEL7.7-PAYG" ] 
+echo "Plan you selected : " ${PLAN} | adddate >> eap.log
+if [ ${PLAN} == "JBoss-EAP7.2-on-RHEL7.7-PAYG" ]
 then
 echo "Initial JBoss EAP 7.2 setup" | adddate >> eap.log
 echo "subscription-manager register --username RHSM_USER --password RHSM_PASSWORD" | adddate >> eap.log
@@ -36,6 +36,100 @@ echo "yum-config-manager --disable rhel-7-server-htb-rpms" | adddate >> eap.log
 yum-config-manager --disable rhel-7-server-htb-rpms | adddate >> eap.log
 
 echo "Installing JBoss EAP 7.2 repos" | adddate >> eap.log
+echo "yum groupinstall -y jboss-eap7" | adddate >> eap.log
+yum groupinstall -y jboss-eap7 >> eap.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP installation Failed" | adddate >> eap.log; exit $flag;  fi
+
+echo "Start JBoss-EAP service" | adddate >> eap.log
+echo "systemctl enable eap7-standalone.service" | adddate >> eap.log
+systemctl enable eap7-standalone.service | adddate >> eap.log 2>&1
+echo "echo "WILDFLY_OPTS=-Djboss.bind.address.management=0.0.0.0" >> ${EAP_RPM_CONF_STANDALONE}" | adddate >> eap.log
+echo 'WILDFLY_OPTS="-Djboss.bind.address.management=0.0.0.0"' >> ${EAP_RPM_CONF_STANDALONE} | adddate >> eap.log 2>&1
+
+echo "systemctl restart eap7-standalone.service" | adddate >> eap.log
+systemctl restart eap7-standalone.service | adddate >> eap.log 2>&1
+echo "systemctl status eap7-standalone.service" | adddate >> eap.log
+systemctl status eap7-standalone.service | adddate >> eap.log 2>&1
+
+# Open Red Hat software firewall for port 8080 and 9990:
+echo "firewall-cmd --zone=public --add-port=8080/tcp --permanent" | adddate >> eap.log
+firewall-cmd --zone=public --add-port=8080/tcp --permanent | adddate >> eap.log 2>&1
+echo "firewall-cmd --zone=public --add-port=9990/tcp --permanent" | adddate >> eap.log
+firewall-cmd --zone=public --add-port=9990/tcp --permanent | adddate  >> eap.log 2>&1
+echo "firewall-cmd --reload" | adddate >> eap.log
+firewall-cmd --reload | adddate >> eap.log 2>&1
+
+# Open Red Hat software firewall for port 22:
+echo "firewall-cmd --zone=public --add-port=22/tcp --permanent" | adddate >> eap.log
+firewall-cmd --zone=public --add-port=22/tcp --permanent | adddate >> eap.log 2>&1
+echo "firewall-cmd --reload" | adddate >> eap.log
+firewall-cmd --reload | adddate >> eap.log 2>&1
+fi
+
+if [ ${PLAN} == "JBoss-EAP7.2-on-RHEL8.0-PAYG" ]
+then
+echo "Initial JBoss EAP 7.2 setup" | adddate >> eap.log
+echo "subscription-manager register --username RHSM_USER --password RHSM_PASSWORD" | adddate >> eap.log
+subscription-manager register --username $RHSM_USER --password $RHSM_PASSWORD >> eap.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Red Hat Subscription Manager Registration Failed" | adddate >> eap.log; exit $flag;  fi
+echo "subscription-manager attach --pool=EAP_POOL" | adddate  >> eap.log
+subscription-manager attach --pool=${RHSM_POOL} >> eap.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Pool Attach for JBoss EAP Failed" | adddate  >> eap.log; exit $flag;  fi
+echo "Subscribing the system to get access to JBoss EAP 7.2 repos" | adddate >> eap.log
+
+# Install JBoss EAP 7.2
+echo "subscription-manager repos --enable=jb-eap-7.2-for-rhel-8-x86_64-rpms" | adddate >> eap.log
+subscription-manager repos --enable=jb-eap-7.2-for-rhel-8-x86_64-rpms >> eap.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Enabling repos for JBoss EAP Failed" | adddate >> eap.log; exit $flag;  fi
+
+echo "Installing JBoss EAP 7.2 repos" | adddate >> eap.log
+echo "yum groupinstall -y jboss-eap7" | adddate >> eap.log
+yum groupinstall -y jboss-eap7 >> eap.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP installation Failed" | adddate >> eap.log; exit $flag;  fi
+
+echo "Start JBoss-EAP service" | adddate >> eap.log
+echo "systemctl enable eap7-standalone.service" | adddate >> eap.log
+systemctl enable eap7-standalone.service | adddate >> eap.log 2>&1
+echo "echo "WILDFLY_OPTS=-Djboss.bind.address.management=0.0.0.0" >> ${EAP_RPM_CONF_STANDALONE}" | adddate >> eap.log
+echo 'WILDFLY_OPTS="-Djboss.bind.address.management=0.0.0.0"' >> ${EAP_RPM_CONF_STANDALONE} | adddate >> eap.log 2>&1
+
+echo "systemctl restart eap7-standalone.service" | adddate >> eap.log
+systemctl restart eap7-standalone.service | adddate >> eap.log 2>&1
+echo "systemctl status eap7-standalone.service" | adddate >> eap.log
+systemctl status eap7-standalone.service | adddate >> eap.log 2>&1
+
+# Open Red Hat software firewall for port 8080 and 9990:
+echo "firewall-cmd --zone=public --add-port=8080/tcp --permanent" | adddate >> eap.log
+firewall-cmd --zone=public --add-port=8080/tcp --permanent | adddate >> eap.log 2>&1
+echo "firewall-cmd --zone=public --add-port=9990/tcp --permanent" | adddate >> eap.log
+firewall-cmd --zone=public --add-port=9990/tcp --permanent | adddate  >> eap.log 2>&1
+echo "firewall-cmd --reload" | adddate >> eap.log
+firewall-cmd --reload | adddate >> eap.log 2>&1
+
+# Open Red Hat software firewall for port 22:
+echo "firewall-cmd --zone=public --add-port=22/tcp --permanent" | adddate >> eap.log
+firewall-cmd --zone=public --add-port=22/tcp --permanent | adddate >> eap.log 2>&1
+echo "firewall-cmd --reload" | adddate >> eap.log
+firewall-cmd --reload | adddate >> eap.log 2>&1
+fi
+
+if [ ${PLAN} == "JBoss-EAP7.3-on-RHEL8.0-PAYG" ]
+then
+echo "Initial JBoss EAP 7.3 setup" | adddate >> eap.log
+echo "subscription-manager register --username RHSM_USER --password RHSM_PASSWORD" | adddate >> eap.log
+subscription-manager register --username $RHSM_USER --password $RHSM_PASSWORD >> eap.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Red Hat Subscription Manager Registration Failed" | adddate >> eap.log; exit $flag;  fi
+echo "subscription-manager attach --pool=EAP_POOL" | adddate  >> eap.log
+subscription-manager attach --pool=${RHSM_POOL} >> eap.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Pool Attach for JBoss EAP Failed" | adddate  >> eap.log; exit $flag;  fi
+echo "Subscribing the system to get access to JBoss EAP 7.3 repos" | adddate >> eap.log
+
+# Install JBoss EAP 7.3
+echo "subscription-manager repos --enable=jb-eap-7.3-for-rhel-8-x86_64-rpms" | adddate >> eap.log
+subscription-manager repos --enable=jb-eap-7.3-for-rhel-8-x86_64-rpms >> eap.log 2>&1
+flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! Enabling repos for JBoss EAP Failed" | adddate >> eap.log; exit $flag;  fi
+
+echo "Installing JBoss EAP 7.3 repos" | adddate >> eap.log
 echo "yum groupinstall -y jboss-eap7" | adddate >> eap.log
 yum groupinstall -y jboss-eap7 >> eap.log 2>&1
 flag=$?; if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP installation Failed" | adddate >> eap.log; exit $flag;  fi
